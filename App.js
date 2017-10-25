59f02e6799585c7ff051b10aa6b4252a84fe2f75
Ainor Syahrizal <macintorch@gmail.com>
2017-10-25 12:55:36 +0800

    add and render card and button

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Deck from './src/Deck';
+import { Card, Button } from 'react-native-elements';
 
 
 const DATA = [
@@ -19,7 +20,23 @@ export default class App extends React.Component {
 
   renderCard(item) {
     return(
-      <Text>{item.text}</Text>
+     <Card
+     key={item.id }
+      title={item.text}
+      image={{ uri: item.uri }}
+     >
+
+     <Text style={{ marginBottom: 10 }}>
+     I can customize the Card further.
+     </Text>
+
+     <Button
+      icon={{ name: 'code' }}
+      backgroundColor="#03A9F4"
+      title="View Now!"
+     />
+
+     </Card>
       );
   }
 
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # react-native-swiipe
 
 Add https://github.com/react-native-training/react-native-elements
+
+$ npm install react-native-elements --save
