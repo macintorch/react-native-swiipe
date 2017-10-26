86c78fa1c8c907a6ad5f003d52125b84644728af
Ainor Syahrizal <macintorch@gmail.com>
2017-10-26 23:32:00 +0800

    advancing the deck

--- src/Deck.js
+++ src/Deck.js
@@ -58,6 +58,12 @@ class Deck extends Component {
 		const item = data[this.state.index];
 
 		direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+
+		// reset card position to (0,0)
+		this.state.position.setValue({ x: 0, y:0 });
+
+		//reset the index
+		this.setState({ index: this.state.index + 1 });
 	}
 
 	resetPosition() {
@@ -82,8 +88,11 @@ class Deck extends Component {
 	}
 
 	renderCards() {
-		return this.props.data.map((item, index) => {
-			if (index === 0) {
+		return this.props.data.map((item, i) => {
+
+			if(i < this.state.index) { return null;}
+
+			if (i === this.state.index) {
 				return (
 						<Animated.View
 							key={item.id}
